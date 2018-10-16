f2541f6616928f602431cfe12ffa4379d739f42e
efe tiryaki <m.efetiryaki@gmail.com>
2018-10-17 00:34:56 +0800

    the limits are added to the max velocity

--- gopigo_controller/scripts/move_gopigo.py
+++ gopigo_controller/scripts/move_gopigo.py
@@ -15,7 +15,24 @@ def move(v,w,t):
     d = 0.06
     v_l = int(scale*(v + d*w))
     v_r = int(scale*(v - d*w))
-    print(v_l,v_r)
+    if v_l>127:
+        v_l = 127
+        print("v_l_max : " + str(v_l))
+    elif v_l<127:
+        v_l =-127
+        print("v_l_min : " + str(v_l))
+    else:
+        print("v_l : " + str(v_l))
+
+    if v_r>127:
+        v_r = 127
+        print("v_r_max : " + str(v_r))
+    elif v_r<127:
+        v_r =-127
+        print("v_r_min : " + str(v_r))
+    else:
+        print("v_r : " + str(v_r))
+
     rate = rospy.Rate(10)
 
     time_start = rospy.get_time()
@@ -43,3 +60,5 @@ if __name__ == "__main__":
             w = -1
         print(v,w)
         move(v,w,t)
+    else:
+        print("there should be 3 argument (v,w,t)\nv : linear velocity\nw : angular velocity\nt : execution time")
