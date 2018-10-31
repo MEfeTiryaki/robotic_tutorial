5a72a0cb11f483540238d7b319e6f14adcdebec0
efe tiryaki <m.efetiryaki@gmail.com>
2018-10-31 21:14:44 +0800

    small changes

--- gopigo_controller/scripts/ball_tracker.py
+++ gopigo_controller/scripts/ball_tracker.py
@@ -28,10 +28,12 @@ def robotPoseCallback(msg):
                  ,msg.pose.orientation.z
                  ,msg.pose.orientation.w]
     robotPose[2] = -tr.euler_from_quaternion(quaternion)[2]
+    print("robot",robotPose)
 
 def ballPoseCallback(msg):
-    ballPosition[0] = msg.pose.position.x
+    ballPosition[0] = -msg.pose.position.x
     ballPosition[1] = msg.pose.position.y
+    print("ball",ballPosition)
 
 
 def track():
@@ -53,7 +55,24 @@ def track():
 
 def calculateControlInput():
     # XXX : WRITE YOUR CONTROL LAW HERE
+    if robotPose[0]!=0 and ballPosition[0]!=0:
+        robotToBall= ballPosition - robotPose[0:2]
+        print("robotToBall : ", robotToBall)
+        robotToBallUnit= robotToBall/np.linalg.norm(robotToBall)
+        print("robotToBallUnit : ", robotToBallUnit)
+        angle = np.arctan2(robotToBallUnit[1],robotToBallUnit[0])
+        print("angle : ", angle)
 
+        # ANGLE ERROR
+        del_angle = angle-robotPose[2]
+
+        w = k_rot * del_angle
+    else:
+        v = 0
+        w = 0
+
+    print("------------------------")
+    v = 0
     # OpenLoop wheel speed calculation
     v_l = int(scale*(v + d*w))
     v_r = int(scale*(v - d*w))
@@ -61,21 +80,23 @@ def calculateControlInput():
     max_vel = 90
     if v_l>max_vel:
         v_l = max_vel
-        print("v_l_max : " + str(v_l))
+        #print("v_l_max : " + str(v_l))
     elif v_l<-max_vel:
         v_l = -max_vel
-        print("v_l_min : " + str(v_l))
+        #print("v_l_min : " + str(v_l))
     else:
-        print("v_l : " + str(v_l))
+        pass
+        #print("v_l : " + str(v_l))
 
     if v_r>max_vel:
         v_r = max_vel
-        print("v_r_max : " + str(v_r))
+        #print("v_r_max : " + str(v_r))
     elif v_r<-max_vel:
         v_r = -max_vel
-        print("v_r_min : " + str(v_r))
+        #print("v_r_min : " + str(v_r))
     else:
-        print("v_r : " + str(v_r))
+        pass
+        #print("v_r : " + str(v_r))
 
 
     return v_l,v_r
